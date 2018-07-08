675c187c230aee38c2826a3bd52222cc39cd8148
jsyarnall <39835944+jsyarnall@users.noreply.github.com>
2018-07-08 16:29:22 -0400

    Adding entire repo

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'f25bbb3153314737a095c78cee716bce'
+});
+
+const handleAPICall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleAPICall
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js')
+const signin = require('./controllers/signin.js')
+const profile = require('./controllers/profile.js')
+const image = require('./controllers/image.js')
+
+const db = knex ({  
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '4367#jack',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) 
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageURL', (req, res) => { image.handleAPICall(req, res) })
+
+
+app.listen(3000, () => {
+	console.log('App is running on Port 3000.');
+})
